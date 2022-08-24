c7903d8919ae45265f4afcf726083b275720a390
Sebastião Leal <sebastiaodeoleal@gmail.com>
2022-08-23 22:11:34 -0300

    aula 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -24,7 +25,8 @@ import { SobreComponent } from './paginas/sobre/sobre.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/paginas/form/form.component.ts
+++ src/app/paginas/form/form.component.ts
@@ -1,5 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Cep } from 'src/app/models/cep';
 import { Cliente } from 'src/app/models/cliente';
+import { CepServico } from 'src/app/servicos/cep-servico';
+
+import { ClienteServico } from '../../servicos/cliente-servico';
 
 @Component({
   selector: 'app-form',
@@ -8,26 +13,36 @@ import { Cliente } from 'src/app/models/cliente';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
-  public cliente:Cliente = {
-    id: 2,
-    nome: "Sebastião",
-    email: "sb@teste.com",
-    senha: "123456",
-    cep: "0993410",
-    endereco: "Rua Gaivota",
-    numero: 12,
-    bairro: "Campanário",
-    cidade: "Diadema",
-    estado: "SP",
-  } as Cliente
+  public cliente:Cliente = {} as Cliente
+  public clientes:Cliente[] = ClienteServico.all()
+  public cep:Cep|undefined = {} as Cep
 
   public mostrarModelo(){
-    console.log(this.cliente)
+    console.log(this.cliente);
+  }
+
+  public salvar() {
+    ClienteServico.salvar(this.cliente);
+    this.listarCLientes();
+  }
+
+  private listarCLientes() {
+    this.clientes = ClienteServico.all();
+  }
+
+  public async buscarViaCep(){
+    this.cep = await new CepServico(this.http).getViaCep(this.cliente.cep);
+    if(this.cep){
+      this.cliente.endereco = this.cep.logradouro,
+      this.cliente.bairro = this.cep.bairro,
+      this.cliente.cidade = this.cep.localidade,
+      this.cliente.estado = this.cep.uf
+    }
   }
 
 }
